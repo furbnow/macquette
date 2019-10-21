36e9fb33027d4476456c2bc7d7ca98ab2cfd1271
Paul Fawkesley <paul@paulfurley.com>
2019-10-21 15:27:58 +0100

    simplfy test_assessment_permissions.py

--- mhep/mhep/assessments/tests/views/test_assessment_permissions.py
+++ mhep/mhep/assessments/tests/views/test_assessment_permissions.py
@@ -10,10 +10,8 @@ class AssessmentPermissionTestsMixin():
         assessment = AssessmentFactory.create()
         self.client.force_authenticate(assessment.owner)
 
-        self.call_endpoint_and_assert(
-            assessment,
-            True,
-        )
+        response = self._call_endpoint(assessment)
+        self._assert_success(response)
 
     def test_organisation_member_who_isnt_owner_can_access(self):
         organisation = OrganisationFactory.create()
@@ -24,19 +22,17 @@ class AssessmentPermissionTestsMixin():
 
         self.client.force_authenticate(org_member)
 
-        self.call_endpoint_and_assert(
-            assessment,
-            True,
-        )
+        response = self._call_endpoint(assessment)
+        self._assert_success(response)
 
     def test_unauthenticated_user_cannot_access(self):
         assessment = AssessmentFactory.create()
 
-        self.call_endpoint_and_assert(
-            assessment,
-            False,
-            "Authentication credentials were not provided.",
+        response = self._call_endpoint(assessment)
+        self._assert_error(
+            response,
             status.HTTP_403_FORBIDDEN,
+            "Authentication credentials were not provided.",
         )
 
     def test_user_who_isnt_owner_and_isnt_organisation_member_cannot_access(self):
@@ -46,62 +42,53 @@ class AssessmentPermissionTestsMixin():
 
         self.client.force_authenticate(non_owner)
 
-        self.call_endpoint_and_assert(
-            assessment,
-            False,
-            "Not found.",
+        response = self._call_endpoint(assessment)
+        self._assert_error(
+            response,
             status.HTTP_404_NOT_FOUND,
+            "Not found.",
         )
 
 
 class TestGetAssessmentPermissions(AssessmentPermissionTestsMixin, APITestCase):
-    def call_endpoint_and_assert(self, assessment, expect_permit, *args):
-        response = self.client.get("/api/v1/assessments/{}/".format(assessment.id))
+    def _call_endpoint(self, assessment):
+        return self.client.get("/api/v1/assessments/{}/".format(assessment.id))
 
-        if expect_permit:
-            assert status.HTTP_200_OK == response.status_code
+    def _assert_success(self, response):
+        assert status.HTTP_200_OK == response.status_code
 
-        if len(args) > 0:
-            expected_error_detail = args[0]
-            assert {"detail": expected_error_detail} == response.json()
-        if len(args) > 1:
-            expected_status_code = args[1]
-            assert expected_status_code == response.status_code
+    def _assert_error(self, response, expected_status_code, expected_error_detail):
+        assert expected_status_code == response.status_code
+        assert {"detail": expected_error_detail} == response.json()
 
 
 class TestUpdateAssessmentPermissions(AssessmentPermissionTestsMixin, APITestCase):
-    def call_endpoint_and_assert(self, assessment, expect_permit, *args):
+    def _call_endpoint(self, assessment):
         update_fields = {
             "data": {"new": "data"},
         }
 
-        response = self.client.patch(
+        return self.client.patch(
             "/api/v1/assessments/{}/".format(assessment.id),
             update_fields,
             format="json",
         )
 
-        if expect_permit:
-            assert status.HTTP_204_NO_CONTENT == response.status_code
+    def _assert_success(self, response):
+        assert status.HTTP_204_NO_CONTENT == response.status_code
 
-        if len(args) > 0:
-            expected_error_detail = args[0]
-            assert {"detail": expected_error_detail} == response.json()
-        if len(args) > 1:
-            expected_status_code = args[1]
-            assert expected_status_code == response.status_code
+    def _assert_error(self, response, expected_status_code, expected_error_detail):
+        assert expected_status_code == response.status_code
+        assert {"detail": expected_error_detail} == response.json()
 
 
 class TestDeleteAssessmentPermissions(AssessmentPermissionTestsMixin, APITestCase):
-    def call_endpoint_and_assert(self, assessment, expect_permit, *args):
-        response = self.client.delete("/api/v1/assessments/{}/".format(assessment.id))
+    def _call_endpoint(self, assessment):
+        return self.client.delete("/api/v1/assessments/{}/".format(assessment.id))
 
-        if expect_permit:
-            assert status.HTTP_204_NO_CONTENT == response.status_code
+    def _assert_success(self, response):
+        assert status.HTTP_204_NO_CONTENT == response.status_code
 
-        if len(args) > 0:
-            expected_error_detail = args[0]
-            assert {"detail": expected_error_detail} == response.json()
-        if len(args) > 1:
-            expected_status_code = args[1]
-            assert expected_status_code == response.status_code
+    def _assert_error(self, response, expected_status_code, expected_error_detail):
+        assert expected_status_code == response.status_code
+        assert {"detail": expected_error_detail} == response.json()
