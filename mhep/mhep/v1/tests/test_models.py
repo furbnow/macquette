85e90d7c840b798d65d085e629467fa4a06ca0f7
Paul Fawkesley <paul@paulfurley.com>
2019-11-04 11:42:05 +0000

    move test for User.organisations out of User
    it's a related_name test, defined in the Organisation rather than the
    User, so it should be tested where it's defined

--- mhep/mhep/v1/tests/test_models.py
+++ mhep/mhep/v1/tests/test_models.py
@@ -1,5 +1,7 @@
 import pytest
 
+from django.conf import settings
+
 from ..models import Organisation
 
 pytestmark = pytest.mark.django_db
@@ -7,3 +9,7 @@ pytestmark = pytest.mark.django_db
 
 def test_organisation_assessments(organisation_with_extras: Organisation):
     assert organisation_with_extras.assessments.all().count() == 1
+
+
+def test_organisations_related_name_on_user_model(user_with_org: settings.AUTH_USER_MODEL):
+    assert user_with_org.organisations.all().count() == 1
