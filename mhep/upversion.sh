b8dbda3277fb994b55301b71f735fdcfaab059ba
Anna Sidwell <anna@takkaria.org>
2020-01-06 22:06:30 +0100

    Update version minting script

--- mhep/upversion.sh
+++ mhep/upversion.sh
@@ -12,9 +12,9 @@ check_django_working() {
 get_latest_version() {
     ls -al mhep/
     echo
-    echo "What's the latest version number? (e.g. v2)"
+    echo "What's will the new version be called? (e.g. v3)"
 
-    read CURRENT_V
+    read NEW_V
 }
 
 
@@ -43,13 +43,13 @@ rename_subdirectories() {
 add_to_django_installed_apps() {
 	echo "Adding ${NEW_V} to config/settings/base.py"
 
-	sed -i "s|    # Your stuff: custom apps go here|    # Your stuff: custom apps go here\n    \"mhep.${NEW_V}.apps.AssessmentsConfig\",|g" config/settings/base.py
+	${SED} -i "s|    # Your stuff: custom apps go here|    # Your stuff: custom apps go here\n    \"mhep.${NEW_V}.apps.AssessmentsConfig\",|g" config/settings/base.py
 }
 
 add_to_django_urls() {
 	echo "Adding ${NEW_V} to config/urls.py"
 
-	sed -i "s|    # Add app versions after this line|    # Add app versions after this line\n    path(\"${NEW_V}/\", include(\"mhep.${NEW_V}.urls\", namespace=\"${NEW_V}\")),|g" config/urls.py
+	${SED} -i "s|    # Add app versions after this line|    # Add app versions after this line\n    path(\"${NEW_V}/\", include(\"mhep.${NEW_V}.urls\", namespace=\"${NEW_V}\")),|g" config/urls.py
 }
 
 create_initial_migration() {
@@ -63,10 +63,12 @@ create_initial_migration() {
 update_fixtures() {
 	echo "Updating fixtures for ${NEW_V}"
 
-	sed -i "s|\"model\": \"${CURRENT_V}\.|\"model\": \"${NEW_V}.|g" mhep/${NEW_V}/fixtures/*.json
+	${SED} -i "s|\"model\": \"${CURRENT_V}\.|\"model\": \"${NEW_V}.|g" mhep/${NEW_V}/fixtures/*.json
 }
 
-NEW_V="dev"
+# Needs GNU sed.
+SED=${SED:=sed}
+CURRENT_V="dev"
 check_django_working
 get_latest_version
 check_destination_directory_doesnt_exist
