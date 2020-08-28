bc3e829559b2597074b20c4e70faab4bed2458d7
Peter Armstrong <peter@carbon.coop>
2020-08-28 15:36:00 +0200

    Low-energy lighting button works again (#476)
    For some reason, low-energy lighting is an 'extended library' which
    means it's not stored in the database and it doesn't have an editing
    function.  Since the standard library file was removed when moving to
    totally database-backed libraries, it got deleted too.  Restore it.

---
+++ mhep/mhep/dev/static/dev/js/library-r6.js
@@ -0,0 +1,19 @@
+//The items in the extendeed library are not added to the list in the library helper (and not displayed in the manager, etc)
+var extended_library = {
+    lighting_measures: {
+        L01: {
+            name: 'Low Energy Lights',
+            description: 'Replacement low energy light bulbs (LEDs) to replace incandecents and halogens, and, in some cases, fluorecent bulbs.  ',
+            performance: '100 lumens/watt minimum',
+            benefits: 'Carbon, Fuel Bills, Lighting Quality. ',
+            cost_units: 'unit',
+            cost: 4,
+            who_by: 'DIY',
+            disruption: 'LOW',
+            associated_work: 'Electricals and wiring. ',
+            key_risks: 'Changes to wiring etc should be undertaken by a qualified electrican. ',
+            notes: 'Where spotlights or downlighters are being replaced care should be taken in ensuring transformers on existing circuits are suitable for LEDs - if unsure take advice from a qualified electrician. Where compact fluorecents are reaching the end of their life, it is also worth replacing them with LEDs - and this may in many cases result in better quality lighting. ',
+            maintenance: 'Occasional Cleaning, eventual replacement'
+        }
+    }
+};
