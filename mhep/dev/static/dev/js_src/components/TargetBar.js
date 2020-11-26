89faaf9691f2106d4d1e861432c40fe32a5e0ecd
Anna Sidwell <anna@takkaria.org>
2020-11-26 14:19:38 +0100

    Improve factoring of TargetBar
    The Frontend Masters React course said not to have more than one
    component in any given file but it seems ridiculous to have a separate
    file for sub-components that are functionally inseparable from the main
    one.  So I think this is worth documenting somewhere in our React style
    guide (so far nonexistent).
    
    So, split out a Target component instead of having drawLine, which was
    the pre-React name.

--- mhep/dev/static/dev/js_src/components/TargetBar.js
+++ mhep/dev/static/dev/js_src/components/TargetBar.js
@@ -1,92 +1,83 @@
 import React from "react";
 
+const HEIGHT = 40;
+
+function Target({ target, xscale, units }) {
+    const x = target.value * xscale;
+
+    return (
+        <>
+            {x === 0 ? null : (
+                <line
+                    x1={x}
+                    y1="1"
+                    x2={x}
+                    y2={HEIGHT - 1}
+                    className="stroke-beige-200"
+                    strokeDasharray="4.33 4.33"
+                />
+            )}
+            <text x={x + 5} y={HEIGHT - 22} className="fill-beige-200">
+                {target.value} {units}
+            </text>
+            <text x={x + 5} y={HEIGHT - 8} className="fill-beige-200 text-bold">
+                {target.label}
+            </text>
+        </>
+    );
+}
+
 /*
  * Draw a 'target bar'.
  *
  * name - string to label the graph with
+ * width - width of bar graph
  * value - numeric value
  * units - units for value
  * targets - [ {label: "...", value: x}, {label: "...", value: x} ]
  */
 export default function TargetBar({ width, name, value, units, targets }) {
-    const height = 40;
+    const unknown = value === null || isNaN(value) || value == Infinity;
 
     // Always 25% larger than max target or value
     const targetValues = targets.map((t) => t.value);
     const maxval = Math.max(value, ...targetValues) * 1.25;
     const xscale = width / maxval;
 
-    const unknown = isNaN(value) || value == Infinity;
-    let header;
-
-    if (unknown) {
-        targets = [];
-        header = "Not enough data";
-    } else {
-        header = `${value} ${units}`;
-        value = Math.round(value);
-    }
-
-    const drawLine = (target) => {
-        const x = target.value * xscale;
-
-        return (
-            <>
-                {x === 0 ? null :
-                <line
-                    key={name + target.label + "_1"}
-                    x1={x}
-                    y1="1"
-                    x2={x}
-                    y2={height - 1}
-                    className="stroke-beige-200"
-                    strokeDasharray="4.33 4.33"
-                />}
-                <text
-                    key={name + target.label + "_2"}
-                    x={x + 5}
-                    y={height - 22}
-                    className="fill-beige-200"
-                >
-                    {target.value} {units}
-                </text>
-                <text
-                    key={name + target.label + "_3"}
-                    x={x + 5}
-                    y={height - 8}
-                    className="fill-beige-200 text-bold"
-                >
-                    {target.label}
-                </text>
-            </>
-        );
-    };
-
     return (
         <div className="targetbar">
             <div className="targetbar-head">
                 <span>{name}:</span>
-                <span>
-                    <b>{header}</b>
+                <span className="text-bold">
+                    {unknown ? "Not enough data" : `${Math.round(value)} ${units}`}
                 </span>
             </div>
 
-            <svg viewBox={`0 0 ${width} ${height}`} height={height}>
+            <svg viewBox={`0 0 ${width} ${HEIGHT}`} height={HEIGHT}>
                 <rect
                     x="1"
                     y="1"
                     width={width - 2}
-                    height={height - 2}
+                    height={HEIGHT - 2}
                     className="fill-beige-800 stroke-beige-200"
                 />
                 <rect
                     x="2"
                     y="2"
-                    width={value * xscale - 4}
-                    height={height - 4}
+                    width={Math.round(value) * xscale - 4}
+                    height={HEIGHT - 4}
                     className="fill-beige-700"
                 />
-                {targets.map(drawLine)}
+                {unknown
+                    ? []
+                    : targets.map((target) => (
+                          <Target
+                              key={name + target.label + units}
+                              xscale={xscale}
+                              target={target}
+                              units={units}
+                          />
+                      ))}
             </svg>
         </div>
     );
