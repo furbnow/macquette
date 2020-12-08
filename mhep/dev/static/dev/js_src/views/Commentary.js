09e1e941a4987a0798b682ac038d39cf3679a6a0
Anna Sidwell <anna@takkaria.org>
2020-12-08 21:26:55 +0100

    Display orphans

--- mhep/dev/static/dev/js_src/views/Commentary.js
+++ mhep/dev/static/dev/js_src/views/Commentary.js
@@ -6,17 +6,7 @@ import LongTextField from '../components/LongTextField';
 // away later, in favour of getting the right fields off the assssment class.  (When
 // the assessment class has the right fields.)  XXX
 export default function Commentary({ assessment, overviewData }) {
-    // It's possible for scenarios to be deleted.  In this case, the assessor might
-    // legit expect the commentary for it to be kept, because it's entered on a view
-    // that is not inside a scenario.  (We know this from user data.)
-    //
-    // Under the Reactified view + Assessment class we do keep it, but we don't display
-    // it.  For extra credit, we could detect when assessment.commentary.scenarios has
-    // entries in it for deleted scenarios and display then for the assessor to see -
-    // but not allow editing, only deleting. XXX
-
-    // I very much doubt that having the scenario graphics at the bottom of the page
-    // is the most useful arrangement.  XXX
+    const orphans = assessment.commentary.getOrphanedScenarioIds();
 
     return (
         <div>
@@ -57,6 +47,33 @@ export default function Commentary({ assessment, overviewData }) {
                     </LongTextField>
                 ))}
 
+            {orphans
+                ? orphans.map((id) => (
+                      <div
+                          key={`commentary_for_${id}`}
+                          className="bg-lighter mb-30 pa-15"
+                      >
+                          <LongTextField
+                              key={`commentary_for_${id}`}
+                              id={`commentary_for_${id}`}
+                              value={assessment.commentary.getText(id)}
+                              setValue={(val) => assessment.commentary.setText(id, val)}
+                          >
+                              <b>Orphaned commentary for &quot;{id}&quot;</b>
+                          </LongTextField>
+                          <button
+                              className="btn btn--icon"
+                              onClick={() => assessment.commentary.deleteText(id)}
+                          >
+                              <svg viewBox="0 0 1792 1792">
+                                  <use xlinkHref="#iconset-trash"></use>
+                              </svg>
+                              Delete
+                          </button>
+                      </div>
+                  ))
+                : null}
+
             <LongTextField
                 id="decisions"
                 className="mb-30"
@@ -68,6 +85,11 @@ export default function Commentary({ assessment, overviewData }) {
                     investigation and development
                 </b>
             </LongTextField>
+
+            {
+                // I very much doubt that having the scenario graphics at the bottom of the page
+                // is the most useful arrangement.  XXX
+            }
 
             <h2 className="mb-0">Overview of scenarios</h2>
 
