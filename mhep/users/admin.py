14fb963c0c40e789bede8566d39793e399004289
Anna Sidwell <anna@takkaria.org>
2021-02-04 21:40:36 +0100

    Improve the user admin display
    * Show full name & email address along existing fields
    * Include link to user social auth records for cross-referencing
    * Add admin action to set users' usernames = email for consistency
    * Add admin action to set users' full names from their first+last names
      concatenated, deleting the separate fields
    
    This lets us keep the data in a consistent format after this branch gets
    merged.

--- mhep/users/admin.py
+++ mhep/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.html import format_html
 
 from mhep.users.forms import UserChangeForm
 from mhep.users.forms import UserCreationForm
@@ -14,5 +16,70 @@ class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "is_staff", "is_superuser"]
+    list_display = ["username", "name", "email", "_usa", "is_staff", "is_superuser"]
     search_fields = ["name"]
+    actions = ["username_to_email", "fill_in_full_name"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related("social_auth")
+
+    def _usa(self, user):
+        links = [
+            (
+                "<a href='"
+                + reverse("admin:social_django_usersocialauth_change", args=(usa.id,))
+                + "'>Record</a>"
+            )
+            for usa in user.social_auth.all()
+        ]
+
+        return format_html(", ".join(links))
+
+    def username_to_email(self, request, queryset):
+        done = 0
+        no_email = 0
+        already = 0
+
+        for user in queryset:
+            if user.email == "":
+                no_email += 1
+            elif user.username != user.email:
+                user.username = user.email
+                user.save()
+                done += 1
+            else:
+                already += 1
+
+        self.message_user(
+            request,
+            f"{no_email} with no email, {already} already had the same username + email, {done} records updated.",
+        )
+
+    username_to_email.short_description = "Set username to email address"
+
+    def fill_in_full_name(self, request, queryset):
+        done = 0
+        no_name = 0
+        already = 0
+
+        for user in queryset:
+            fullname = f"{user.first_name} {user.last_name}"
+
+            if user.name == " ":
+                no_name += 1
+            elif user.name != "":
+                already += 1
+            elif user.name != fullname:
+                user.name = fullname
+                user.first_name = ""
+                user.last_name = ""
+                user.save()
+                done += 1
+
+        self.message_user(
+            request,
+            f"{no_name} with no name, {already} already had full names, {done} records updated.",
+        )
+
+    fill_in_full_name.short_description = "Set full name field to first+last names"
