929a66deb2e9ba2ae54e07c74c2c61f2ad83ebfe
Anna Sidwell <anna@takkaria.org>
2021-04-14 13:47:04 +0200

    TSify OnOffToggleButton

--- mhep/dev/static/dev/js_src/components/OnOffToggleButton.tsx
+++ mhep/dev/static/dev/js_src/components/OnOffToggleButton.tsx
@@ -1,7 +1,19 @@
-import React, { useContext } from 'react';
+import React, { ReactElement, useContext } from 'react';
 import { UpdateFunction } from '../context/UpdateFunction';
 
-export default function OnOffToggleButton({ onTitle, offTitle, value, setValue }) {
+interface OnOffToggleButtonProps {
+    onTitle: string;
+    offTitle: string;
+    value: boolean;
+    setValue: (val: boolean) => void;
+}
+
+export default function OnOffToggleButton({
+    onTitle,
+    offTitle,
+    value,
+    setValue,
+}: OnOffToggleButtonProps): ReactElement {
     const updateFn = useContext(UpdateFunction);
     return (
         <button
