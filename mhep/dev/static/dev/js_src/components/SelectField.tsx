1cfa06120ace12200e9a7e74d3539143ca9ad0a0
Anna Sidwell <anna@takkaria.org>
2021-04-14 16:57:12 +0200

    Fix the SelectField type errors
    The problem before was that if you put the data you are trying use as a
    value into the DOM and get it out again, you lose all the type info:
    whatever you put in there, you get a 'string' back.  And while we as
    smart humans know this, TS doesn't have a clue.  However, if you use the
    index into the options array as the value in the <option>, you can use
    that to get the right data back out again, and TS knows exactly what
    type it is.

--- mhep/dev/static/dev/js_src/components/SelectField.tsx
+++ mhep/dev/static/dev/js_src/components/SelectField.tsx
@@ -4,33 +4,37 @@ import { UpdateFunction } from '../context/UpdateFunction';
 interface SelectFieldProps<T> {
     id: string;
     options: { value: T; display: string }[];
-    value: T;
+    value: T | null;
     setValue: (val: T) => void;
 }
 
-export default function SelectField<Type>({
+export default function SelectField<T>({
     id,
     options,
     value,
     setValue,
-}: SelectFieldProps<Type>): ReactElement {
+}: SelectFieldProps<T>): ReactElement {
     const updateFn = useContext(UpdateFunction);
+    const current = options.findIndex((row) => row.value === value);
+
     return (
         <select
-            value={value}
             id={`field_${id}`}
+            value={current === -1 ? undefined : current}
             onChange={(evt) => {
-                setValue(evt.target.value);
+                const idx = parseInt(evt.target.value, 10);
+                setValue(options[idx].value);
                 updateFn();
             }}
             onBlur={(evt) => {
-                setValue(evt.target.value);
+                const idx = parseInt(evt.target.value, 10);
+                setValue(options[idx].value);
                 updateFn();
             }}
         >
             <option hidden>Select one...</option>
             {options.map((opt, i) => (
-                <option value={opt.value} key={i}>
+                <option value={i} key={i}>
                     {opt.display}
                 </option>
             ))}
