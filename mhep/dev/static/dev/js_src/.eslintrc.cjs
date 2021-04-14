429e268a8eee864d96ffda7fd704694784d1fb47
Anna Sidwell <anna@takkaria.org>
2021-04-14 17:01:13 +0200

    Make eslint run on ts files

--- mhep/dev/static/dev/js_src/.eslintrc.cjs
+++ mhep/dev/static/dev/js_src/.eslintrc.cjs
@@ -6,24 +6,21 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    'plugin:@typescript-eslint/recommended',
     "plugin:import/errors",
     "plugin:import/typescript",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
     "prettier",
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
     sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint", "react", "import", "jsx-a11y", "react-hooks"],
+  plugins: ["react", "import", "jsx-a11y", "react-hooks"],
   rules: {
     "react/prop-types": 0,
     "no-console": 1,
@@ -32,4 +29,26 @@ module.exports = {
     "no-var": 2,
   },
   settings: { react: { version: "detect" } },
+  overrides: [
+    // typescript
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parser: '@typescript-eslint/parser',
+    },
+
+    // tests
+    {
+      files: ['*.test.js', '*.test.ts', '*.test.tsx'],
+      extends: [
+        "plugin:jest/recommended",
+        "plugin:jest/style",
+      ],
+    },
+  ]
 };
