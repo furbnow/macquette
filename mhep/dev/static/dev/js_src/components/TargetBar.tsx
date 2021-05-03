95c71d0f524d321cb32f68730178f945f839508d
Anna Sidwell <anna@takkaria.org>
2021-05-03 18:18:46 +0200

    Avoid text overlaps on the targetbar text

--- mhep/dev/static/dev/js_src/components/TargetBar.tsx
+++ mhep/dev/static/dev/js_src/components/TargetBar.tsx
@@ -1,53 +1,73 @@
-import React, { ReactElement } from 'react';
+import React, { useState, ReactElement } from 'react';
 
-const HEIGHT = 40;
 const COLOURS = ['fill-beige-700', 'fill-beige-400'];
+const AXIS_COLOUR = 'black';
 
 interface Target {
     label: string;
     value: number;
 }
 
-interface TargetProps {
-    target: Target;
-    y: number;
-    height: number;
-    xscale: number;
-    units: string;
-}
+/**
+ * Calculate x pos + width of target bounding boxes
+ *
+ * Equivalent to the following:
+ *
+ * if (targets.length === 2) {
+ *     return [
+ *         [ xPos[0], xPos[1] - xPos[0] ],
+ *         [ xPos[1], width - xPos[2] - 0.5 ],
+ *     ];
+ * } else if (targets.length === 3) {
+ *     return [
+ *         [ xPos[0], xPos[1] - xPos[0] ],
+ *         [ xPos[1], xPos[2] - xPos[1] ],
+ *         [ xPos[2], width - xPos[2] - 0.5 ],
+ *     ];
+ * } else {
+ *     return [];
+ * }
+ */
+function calculateTargetBoxes(
+    targets: Target[],
+    xscale: number,
+    width: number
+): [number, number][] {
+    /* Special case length === 1 for clarity */
+    if (targets.length === 1) {
+        return [[1, width - 1]];
+    }
 
-function Target({ target, height, y, xscale, units }: TargetProps) {
-    const x = 1.5 + target.value * xscale;
+    const xPos = targets.map((target) => 1 + target.value * xscale);
 
-    return (
-        <>
-            <line x1={x} y1={y} x2={x} y2={y + height / 3} className="stroke-beige-200" />
-            <text x={x + 5} y={y + height - 22} className="fill-beige-200">
-                {target.value} {units}
-            </text>
-            <text x={x + 5} y={y + height - 8} className="fill-beige-200 text-bold">
-                {target.label}
-            </text>
-        </>
-    );
+    return xPos.map((pos, idx) => {
+        if (idx === targets.length - 1) {
+            return [pos, width - pos - 0.5];
+        } else {
+            return [pos, xPos[idx + 1] - pos];
+        }
+    });
 }
 
 interface TargetBarProps {
+    /** Width of bar graph */
     width: number;
+    /** Graph label */
     name: string;
+    /** Value or array of values to display */
     value: number | number[];
+    /** Units for values */
     units: string;
+    /** Array of label, value pairs for targets */
     targets: Target[];
 }
 
 /**
- * Draw a 'target bar'.
+ * Draw a horizontal mini bar graph with specified axis markers ('targets').
  *
- * @param name - string to label the graph with
- * @param width - width of bar graph
- * @param value - numeric value
- * @param units - units for value
- * @param targets - [ {label: "...", value: x}, {label: "...", value: x} ]
+ * Calculate the bounding box for the text of each target and use it as a clipping path
+ * so that the text doesn't mush up and look really ugly.  Still doesn't look great but
+ * it's an improvement over what came before.
  */
 export default function TargetBar({
     width,
@@ -56,6 +76,8 @@ export default function TargetBar({
     units,
     targets,
 }: TargetBarProps): ReactElement {
+    const [id] = useState(Math.floor(Math.random() * 255));
+
     const values = !Array.isArray(value) ? [value] : value;
     const unknown =
         values.filter((val) => val === null || isNaN(val) || val == Infinity).length > 0;
@@ -78,19 +100,13 @@ export default function TargetBar({
     const targetsHeight = 38;
     const totalHeight = barsHeight + axisHeight + targetsHeight;
 
+    const targetBoxes = calculateTargetBoxes(targets, xscale, width);
+
     return (
         <div className="targetbar">
             <div className="targetbar-head">
                 <span>{name}</span>
-                {unknown ? (
-                    <span className="text-bold">Not enough data</span>
-                ) : (
-                    values.length === 1 && (
-                        <span className="text-bold">
-                            {Math.round(value)} {units}
-                        </span>
-                    )
-                )}
+                {unknown && <span className="text-bold">Not enough data</span>}
             </div>
 
             <svg
@@ -107,7 +123,7 @@ export default function TargetBar({
                             height={barHeight}
                             className={COLOURS[idx]}
                         />
-                        {values.length > 1 && !unknown && (
+                        {!unknown && (
                             <text
                                 x={4 + Math.round(val) * xscale}
                                 y={11.5 + barHeight * idx}
@@ -124,29 +140,75 @@ export default function TargetBar({
                     x1={1.5}
                     y1={1.5}
                     x2={1.5}
-                    y2={barsHeight}
-                    className="stroke-beige-200"
+                    y2={barsHeight + 0.5}
+                    stroke={AXIS_COLOUR}
                 />
 
                 {/* Horizontal axis line */}
                 <line
                     x1={1}
-                    y1={barsHeight + 0.5}
+                    y1={barsHeight + 1}
                     x2={width}
-                    y2={barsHeight + 1.5}
-                    className="stroke-beige-200"
+                    y2={barsHeight + 1}
+                    stroke={AXIS_COLOUR}
                 />
 
-                {targets.map((target, idx) => (
-                    <Target
-                        key={idx}
-                        y={barsHeight + axisHeight}
-                        height={targetsHeight}
-                        xscale={xscale}
-                        target={target}
-                        units={units}
-                    />
+                {targetBoxes.map(([xpos, width], idx) => (
+                    <clipPath key={idx} id={`clip-target-${id}-${idx}`}>
+                        <rect
+                            x={xpos}
+                            y={barsHeight + axisHeight}
+                            width={width - 1}
+                            height={targetsHeight - 0.5}
+                        />
+                    </clipPath>
                 ))}
+
+                {/* Useful for debugging so leaving in here.
+
+                targetBoxes.map(([xpos, width], idx) => (
+                    <rect
+                        idx={idx}
+                        x={xpos}
+                        y={barsHeight + axisHeight}
+                        width={width}
+                        height={targetsHeight - 0.5}
+                        stroke={AXIS_COLOUR}
+                        fill="transparent"
+                    />
+                ))*/}
+
+                {targets.map((target, idx) => {
+                    const x = targetBoxes[idx][0];
+                    const y = barsHeight + axisHeight;
+                    const height = targetsHeight;
+
+                    return (
+                        <g key={idx} clipPath={`url(#clip-target-${id}-${idx})`}>
+                            <line
+                                x1={x + 0.5}
+                                y1={y}
+                                x2={x + 0.5}
+                                y2={y + height / 3}
+                                stroke={AXIS_COLOUR}
+                            />
+                            <text
+                                x={x + 5}
+                                y={y + height - 22}
+                                className="fill-beige-200"
+                            >
+                                {target.value} {units}
+                            </text>
+                            <text
+                                x={x + 5}
+                                y={y + height - 8}
+                                className="fill-beige-200 text-bold"
+                            >
+                                {target.label}
+                            </text>
+                        </g>
+                    );
+                })}
             </svg>
         </div>
     );
