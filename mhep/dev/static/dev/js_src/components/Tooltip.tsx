08860244a462c3d3040b026b836db475a9ec8f0a
Thomas Hoddinott <thomas@carbon.coop>
2021-05-24 15:29:41 +0100

    Use SVG for tooltip marker

--- mhep/dev/static/dev/js_src/components/Tooltip.tsx
+++ mhep/dev/static/dev/js_src/components/Tooltip.tsx
@@ -1,9 +1,20 @@
 import React, { ReactElement } from 'react';
+import { Info } from './icons/info';
 
 interface TooltipProps {
     children: string;
 }
 
 export default function Tooltip({ children }: TooltipProps): ReactElement {
-    return <i className="icon-question-sign" title={children}></i>;
+    return (
+        <span title={children}>
+            <Info
+                style={{
+                    color: '#5e5e5e',
+                    marginLeft: '6px',
+                    verticalAlign: 'text-bottom',
+                }}
+            />
+        </span>
+    );
 }
