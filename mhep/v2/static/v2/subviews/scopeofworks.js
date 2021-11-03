e3f8c28172cfb7b2f05abbd36da18e7433742b16
Anna Sidwell <anna@takkaria.org>
2021-11-03 16:02:08 +0100

    Implement further export for scope of works (#744)

--- mhep/v2/static/v2/subviews/scopeofworks.js
+++ mhep/v2/static/v2/subviews/scopeofworks.js
@@ -1,6 +1,15 @@
 console.log('Debug scopeofworks.js');
 
 function scopeofworks_initUI() {
+    if (!window.questionnaire) {
+        $.ajax({
+            url: urlHelper.static('subviews/householdquestionnaire.js'),
+            dataType: 'script',
+            async: false,
+            error: handleServerError('loading householdquestionnaire.js'),
+        });
+    }
+
     $('#export_csv').click(function () {
         const assessment = p.data;
         const measures = getMeasures(assessment);
@@ -8,18 +17,61 @@ function scopeofworks_initUI() {
         const csv = convertObjectArrayToCSV(formatted);
         downloadCSV(csv, 'measures.csv');
     });
+
+
+    function get_lookup(table, key) {
+        return table[project.master.household[key]];
+    }
+
+    $('#export_csv_2').click(function () {
+        const assessment = p.data;
+        const outputFields = {
+            house_type: questionnaire.HOUSE_TYPE[project.master.household.house_type],
+            house_nr_bedrooms: project.master.household.house_nr_bedrooms,
+            construct_note_floors: project.master.household.construct_note_floors,
+            construct_note_floors: project.master.household.construct_note_floors,
+            construct_note_roof: project.master.household.construct_note_roof,
+            construct_note_openings: project.master.household.construct_note_openings,
+            construct_note_drainage: project.master.household.construct_note_drainage,
+            construct_note_ventiliation: project.master.household.construct_note_ventiliation,
+            construct_note_ingress: project.master.household.construct_note_ingress,
+            structural_issues: project.master.household.structural_issues,
+            structural_issues_note: project.master.household.structural_issues_note,
+            damp: project.master.household.damp,
+            damp_note: project.master.household.damp_note,
+            ventilation_adequate_paths: questionnaire.YES_NO_NA[project.master.household.ventilation_adequate_paths],
+            ventilation_purge_vents: questionnaire.YES_NO_NA[project.master.household.ventilation_purge_vents],
+            ventilation_gaps: questionnaire.YES_NO_NA[project.master.household.ventilation_gaps],
+            ventilation_note: project.master.household.ventilation_note,
+            ventilation_suggestion: questionnaire.VENTILATION_SUGGESTION[project.master.household.ventilation_suggestion],
+            radon_risk: questionnaire.RADON_RISK[project.master.household.radon_risk],
+            flooding_rivers_sea: questionnaire.FLOODING_RISK[project.master.household.flooding_rivers_sea],
+            flooding_surface_water: questionnaire.FLOODING_RISK[project.master.household.flooding_surface_water],
+            flooding_reservoirs: questionnaire.FLOODING_RISK_RESERVOIRS[project.master.household.flooding_reservoirs],
+            overheating_note: project.master.household.overheating_note,
+            historic_age_band: questionnaire.HISTORIC_AGE_BAND[project.master.household.historic_age_band],
+            historic_conserved: questionnaire.YES_NO_NA[project.master.household.historic_conserved],
+            historic_listed: questionnaire.HISTORIC_LISTED[project.master.household.historic_listed],
+            context_and_other_points: project.master.household.context_and_other_points,
+            TFA: project.master.TFA,
+            ventilation_type: project.master.ventilation.ventilation_type,
+            ventilation_name: project.master.ventilation.ventilation_name,
+        };
+        const cells = [
+            Object.keys(outputFields).map(cleanCSVcell),
+            Object.values(outputFields).map(r => r || '').map(cleanCSVcell),
+        ];
+        const csv = cells[0].join(',') + '\n' + cells[1].join(',');
+        downloadCSV(csv, 'scopeofworks.csv');
+    });
 }
 
 function cleanCSVcell(csvCell) {
     let csvCellCleaned = csvCell
         .toString()
-        .replace(/\n/g, '')
         .replace(/"/g, '""')
         .trim();
-    if (csvCellCleaned.includes(',')) {
-        csvCellCleaned = `"${csvCellCleaned}"`;
-    }
-    return csvCellCleaned;
+    return `"${csvCellCleaned}"`;
 }
 
 function convertObjectArrayToCSV(objArray) {
