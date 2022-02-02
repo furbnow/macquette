0589db904a5fc411ddebf75695bb0ff65d1b0ce6
Joe MacMahon <joe@carbon.coop>
2022-02-02 19:06:34 +0000

    client/helpers: use a single symbol in cache getter for easier debugging

--- client/src/v2/helpers/cacheGetter.ts
+++ client/src/v2/helpers/cacheGetter.ts
@@ -1,19 +1,23 @@
+const cacheKey = Symbol.for('cached values for getters');
+
 /** Cache the result of the target getter. Assumes the getter is pure and the values it closes over are immutable. */
 export const cache = <T>(
     _target: unknown,
     propertyKey: string,
     descriptor: TypedPropertyDescriptor<T>,
 ) => {
-    const cachedValueKey = Symbol.for(`cached value for getter ${propertyKey}`);
     if (descriptor.get !== undefined) {
         const innerMethod = descriptor.get;
         descriptor.get = function () {
             /* eslint-disable */
             const obj: any = this;
-            if (obj[cachedValueKey] === undefined) {
-                obj[cachedValueKey] = innerMethod.apply(obj);
+            if (obj[cacheKey] === undefined) {
+                obj[cacheKey] = {};
             }
-            return obj[cachedValueKey];
+            if (obj[cacheKey][propertyKey] === undefined) {
+                obj[cacheKey][propertyKey] = innerMethod.apply(obj);
+            }
+            return obj[cacheKey][propertyKey];
             /* eslint-enable */
         };
     } else {
