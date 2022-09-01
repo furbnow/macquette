30bfbed895a57c00660957c5958a74a06731e155
Anna Sidwell <anna@takkaria.org>
2022-09-01 17:44:48 +0200

    legacy client: move code only used in worksheet view to worksheet view

--- mhep/v2/static/v2/js/misc.js
+++ mhep/v2/static/v2/js/misc.js
@@ -193,24 +193,6 @@ function InitUI() {
         window[functionname]();
     }
 
-    $('.monthly').each(function () {
-
-        var name = $(this).attr('key');
-        var dp = $(this).attr('dp');
-        var title = $(this).attr('title');
-        var units = $(this).attr('units');
-
-        var out = '';
-        var sum = 0;
-        for (var m = 0; m < 12; m++) {
-            out += "<td key='" + name + '.' + m + "' dp=" + dp + " units='" + units + "'></td>";
-            sum += varget(name + '.' + m);
-        }
-        var mean = sum / 12.0;
-
-        $(this).html('<tr><td>' + title + '</td><td>sum:' + sum.toFixed(dp) + '<br>mean:' + mean.toFixed(dp) + '</td>' + out + '</tr>');
-    });
-
     if (page == 'householdquestionnaire') {
         $('.scenario-name').html('Household Questionnaire');
     } else if (page == 'commentary') {
--- mhep/v2/static/v2/subviews/worksheets.js
+++ mhep/v2/static/v2/subviews/worksheets.js
@@ -1,2 +1,18 @@
 function worksheets_initUI(){
+    $('.monthly').each(function () {
+        var name = $(this).attr('key');
+        var dp = $(this).attr('dp');
+        var title = $(this).attr('title');
+        var units = $(this).attr('units');
+
+        var out = '';
+        var sum = 0;
+        for (var m = 0; m < 12; m++) {
+            out += "<td key='" + name + '.' + m + "' dp=" + dp + " units='" + units + "'></td>";
+            sum += varget(name + '.' + m);
+        }
+        var mean = sum / 12.0;
+
+        $(this).html('<tr><td>' + title + '</td><td>sum:' + sum.toFixed(dp) + '<br>mean:' + mean.toFixed(dp) + '</td>' + out + '</tr>');
+    });
 }
