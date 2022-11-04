furbnow/macquette

commit 455391efc3b2a3ecf80e9220c6a9c0cd8379fac2

Significantly improve performance of library list API endpoint
Previously I was noticing 70+ database queries for a single request in
production, which was mostly the same query repeated. The number of
queries was O(n) where n was the number of libraries a user has access
to via an organisational membership.  The number of queries is now O(1),
or more concretely, 6.

- MyLibrariesMixin.my_libraries is refactored and runs only one query.
- ListCreateLibraries.get_queryset() prefetches various related fields
  so it only has to be done once.
- check_library_share_permissions() now makes use of this prefetched
  data instead of fetching it anew for each library

mhep/v2/views/libraries.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from django.db.models import Q
 from rest_framework import exceptions
 from rest_framework import generics
 from rest_framework import status
@@ -23,24 +24,18 @@ STATIC_URLS = build_static_dictionary()
 
 class MyLibrariesMixin:
     def my_libraries(self):
-        user_libraries = getattr(self.request.user, f"{VERSION}_libraries").all()
-        user_orgs = getattr(self.request.user, f"{VERSION}_organisations").all()
+        user = self.request.user
+        user_v_libraries = getattr(user, f"{VERSION}_libraries")
+        user_v_organisations = getattr(user, f"{VERSION}_organisations")
 
-        all_libraries = user_libraries
+        user_libraries = Q(id__in=user_v_libraries.values("id"))
+        org_libraries = Q(owner_organisation__in=user_v_organisations.values("id"))
+        shared_libraries = Q(shared_with__in=user_v_organisations.values("id"))
+        global_libraries = Q(owner_user=None, owner_organisation=None)
 
-        for org in user_orgs:
-            org_libraries = org.libraries.all()
-            all_libraries |= org_libraries
-
-            shared_libraries = org.libraries_shared_with.all()
-            all_libraries |= shared_libraries
-
-        global_libraries = Library.objects.filter(
-            owner_user=None, owner_organisation=None
+        return Library.objects.filter(
+            user_libraries | global_libraries | shared_libraries | org_libraries
         )
-        all_libraries |= global_libraries
-
-        return all_libraries.distinct()
 
 
 class ListCreateLibraries(MyLibrariesMixin, generics.ListCreateAPIView):
@@ -49,7 +44,12 @@ class ListCreateLibraries(MyLibrariesMixin, generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self, *args, **kwargs):
-        return self.my_libraries()
+        return self.my_libraries().prefetch_related(
+            "owner_user",
+            "owner_organisation",
+            "owner_organisation__librarians",
+            "owner_organisation__admins",
+        )
 
 
 class UpdateDestroyLibrary(

mhep/v2/views/helpers.py
@@ -101,17 +101,21 @@ def check_library_write_permissions(library, original_request):
 
 def check_library_share_permissions(library, original_request):
     """
-    manually check the permissions for the ShareUnshareOrganisationLibraries view to work out
-    if the current user (based on original_request) is allowed to share / unshare this library.
+    manually check the permissions for the ShareUnshareOrganisationLibraries view to
+    work out if the current user (based on original_request) is allowed to share /
+    unshare this library.
     """
-
-    from ..views.organisations import (
-        ShareUnshareOrganisationLibraries,
-    )  # avoid circular import
 
     owner_organisation = library.owner_organisation
     if owner_organisation is None:
         return False
+
+    if original_request.user not in owner_organisation.admins.all():
+        return False
+
+    from ..views.organisations import (
+        ShareUnshareOrganisationLibraries,
+    )  # avoid circular import
 
     view = ShareUnshareOrganisationLibraries(
         kwargs={
@@ -120,11 +124,6 @@ def check_library_share_permissions(library, original_request):
             # "otherorgid" not set as it's shouldn't be relevant: we're only checking permission
         }
     )
-
-    # check view-level permissions
-    for permission in view.get_permissions():
-        if not permission.has_permission(original_request, view):
-            return False
 
     # check object-level permissions
     for permission in view.get_permissions():

commit 365a687ccb364a229f9b7cf68ab3f0a0cfbf9791

server: fix bug introduced by 455391ef where libraries could appear multiple times

mhep/v2/views/libraries.py
@@ -33,8 +33,12 @@ class MyLibrariesMixin:
         shared_libraries = Q(shared_with__in=user_v_organisations.values("id"))
         global_libraries = Q(owner_user=None, owner_organisation=None)
 
-        return Library.objects.filter(
-            user_libraries | global_libraries | shared_libraries | org_libraries
+        return (
+            Library.objects.filter(
+                user_libraries | global_libraries | shared_libraries | org_libraries
+            )
+            .distinct()
+            .order_by("id")
         )
 
 

mhep/v2/tests/views/test_libraries.py
@@ -132,6 +132,27 @@ class TestListLibraries(APITestCase):
 
         assert shared_lib.id in retrieved_ids
 
+    def test_library_shared_through_multiple_shared_organisations(self):
+        sharing_org = OrganisationFactory.create()
+
+        my_org1 = OrganisationFactory.create()
+        my_org1.members.add(self.me)
+
+        my_org2 = OrganisationFactory.create()
+        my_org2.members.add(self.me)
+
+        shared_lib = LibraryFactory.create(
+            owner_organisation=sharing_org, owner_user=None
+        )
+        shared_lib.shared_with.add(my_org1)
+        shared_lib.shared_with.add(my_org2)
+
+        self.client.force_authenticate(self.me)
+        response = self.client.get(f"/{VERSION}/api/libraries/")
+        assert response.status_code == status.HTTP_200_OK
+
+        assert 1 == len(response.data)
+
     def test_can_write_is_false_for_org_librarian(self):
         my_org = OrganisationFactory.create()
         my_org.members.add(self.me)