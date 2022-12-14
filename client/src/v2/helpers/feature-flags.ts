35a7d3b5f07dfce0091ff6afc3692d0d48106091
Anna Sidwell <anna@takkaria.org>
2022-12-14 01:38:11 +0100

    client/helpers: fix feature flag class by delaying init until use
    Problem here is that as a singleton, it's initialised at JS load time,
    which will always be before window.flags has been set (which happens
    only after all JS has been loaded).

--- client/src/v2/helpers/feature-flags.ts
+++ client/src/v2/helpers/feature-flags.ts
@@ -8,38 +8,46 @@ import { isIndexable } from './is-indexable';
 class FeatureFlags {
     // Use an ECMAScript private field, rather than TypeScript `private`, so
     // that access is checked at runtime (for legacy JS code).
-    #features: Set<string>;
+    #features: Set<string> | null;
 
     constructor() {
-        if (
-            typeof window !== 'undefined' &&
-            isIndexable(window) &&
-            Array.isArray(window['features']) &&
-            window['features'].every(
-                (item: unknown): item is string => typeof item === 'string',
-            )
-        ) {
-            this.#features = new Set(window['features']);
-        } else {
-            const defaults = new Set(['new-fuvc']);
-            this.#features = defaults;
+        this.#features = null;
+    }
+
+    private getFeatures(): Set<string> {
+        if (this.#features === null) {
+            if (
+                typeof window !== 'undefined' &&
+                isIndexable(window) &&
+                Array.isArray(window['features']) &&
+                window['features'].every(
+                    (item: unknown): item is string => typeof item === 'string',
+                )
+            ) {
+                this.#features = new Set(window['features']);
+            } else {
+                const defaults = new Set(['new-fuvc']);
+                this.#features = defaults;
+            }
         }
+
+        return this.#features;
     }
 
     has(feature: string): boolean {
-        return this.#features.has(feature);
+        return this.getFeatures().has(feature);
     }
 
     #setFeature(feature: string, value: boolean): void {
         if (value) {
-            this.#features.add(feature);
+            this.getFeatures().add(feature);
         } else {
-            this.#features.delete(feature);
+            this.getFeatures().delete(feature);
         }
     }
 
     withFeature<T>(feature: string, value: boolean, fn: () => T): T {
-        const previousFeatureValue = this.#features.has(feature);
+        const previousFeatureValue = this.getFeatures().has(feature);
         this.#setFeature(feature, value);
         try {
             return fn();
