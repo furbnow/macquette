6bdd7f30ae0f7930c3dd5d00f750dd9f28c07dd8
Joe MacMahon <joe@retrofit.coop>
2023-05-19 14:06:18 +0100

    client/ui: make numeric input not use inner component

--- client/src/v2/ui/input-components/number.tsx
+++ client/src/v2/ui/input-components/number.tsx
@@ -18,45 +18,40 @@ export function NumberInput({
     unit,
     ...passthroughProps
 }: NumberInputProps) {
-    // Use a nested inner component to make sure internal state gets reset when
-    // props are updated
-    function Inner() {
-        const [modifiedValue, setModifiedValue] = useState<string | null>(null);
-        const parseable =
-            modifiedValue !== null && /^-?([1-9]\d*|0)(\.\d*[1-9])?$/.test(modifiedValue);
-        const valid = modifiedValue === null || modifiedValue === '' || parseable;
-        function handleBlur() {
-            if (modifiedValue !== null) {
-                if (modifiedValue === '') {
-                    onChange(null);
-                } else if (parseable) {
-                    onChange(parseFloat(modifiedValue));
-                }
+    const [modifiedValue, setModifiedValue] = useState<string | null>(null);
+    const parseable =
+        modifiedValue !== null && /^-?([1-9]\d*|0)(\.\d*[1-9])?$/.test(modifiedValue);
+    const valid = modifiedValue === null || modifiedValue === '' || parseable;
+    function handleBlur() {
+        if (modifiedValue !== null) {
+            if (modifiedValue === '') {
+                onChange(null);
+            } else if (parseable) {
+                onChange(parseFloat(modifiedValue));
             }
         }
-        const style: React.CSSProperties = {
-            ...(styleProp ?? {}),
-            ...(valid ? {} : { borderColor: 'red' }),
-        };
-        let unitFragment: JSX.Element | null = null;
-        if (unit !== undefined) {
-            unitFragment = <>&nbsp;{unit}</>;
-        }
-        const { className, ...rest } = passthroughProps;
-        return (
-            <span style={{ whiteSpace: 'nowrap' }} className={className}>
-                <input
-                    type="text"
-                    className="input--number"
-                    value={modifiedValue ?? outerValue?.toString(10) ?? ''}
-                    onChange={(evt) => setModifiedValue(evt.target.value)}
-                    onBlur={handleBlur}
-                    style={style}
-                    {...rest}
-                />
-                {unitFragment}
-            </span>
-        );
     }
-    return <Inner />;
+    const style: React.CSSProperties = {
+        ...(styleProp ?? {}),
+        ...(valid ? {} : { borderColor: 'red' }),
+    };
+    let unitFragment: JSX.Element | null = null;
+    if (unit !== undefined) {
+        unitFragment = <>&nbsp;{unit}</>;
+    }
+    const { className, ...rest } = passthroughProps;
+    return (
+        <span style={{ whiteSpace: 'nowrap' }} className={className}>
+            <input
+                type="text"
+                className="input--number"
+                value={modifiedValue ?? outerValue?.toString(10) ?? ''}
+                onChange={(evt) => setModifiedValue(evt.target.value)}
+                onBlur={handleBlur}
+                style={style}
+                {...rest}
+            />
+            {unitFragment}
+        </span>
+    );
 }
