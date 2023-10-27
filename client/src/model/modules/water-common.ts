2fa390ee14b0d85ad0053f679ca6d6240c3f58e3
Jo MacMahon <jo@retrofit.coop>
2023-10-27 16:15:12 +0100

    client/model: water common (#1003)

--- client/src/model/modules/water-common.ts
+++ client/src/model/modules/water-common.ts
@@ -35,7 +35,7 @@ export function extractWaterCommonInputFromLegacy(scenario: Scenario): WaterComm
   };
 }
 
-type WaterCommonDependencies = {
+export type WaterCommonDependencies = {
   occupancy: {
     occupancy: number;
   };
---
+++ client/test/model/water-common.test.ts
@@ -0,0 +1,67 @@
+import fc from 'fast-check';
+import { Month } from '../../src/model/enums/month';
+import {
+  WaterCommon,
+  WaterCommonDependencies,
+  WaterCommonInput,
+} from '../../src/model/modules/water-common';
+import { sensibleFloat } from '../arbitraries/legacy-values';
+import { legacyWaterHeating } from './golden-master/water-heating';
+
+const arbWaterCommonInput: fc.Arbitrary<WaterCommonInput> = fc.record({
+  lowWaterUseDesign: fc.boolean(),
+  annualEnergyContentOverride: fc.oneof(fc.constant(false as const), sensibleFloat),
+  solarHotWater: fc.boolean(),
+});
+
+const arbWaterCommonDependencies: fc.Arbitrary<WaterCommonDependencies> = fc.record({
+  occupancy: fc.record({
+    occupancy: sensibleFloat,
+  }),
+});
+
+function makeLegacyDataForWaterCommon(
+  input: WaterCommonInput,
+  dependencies: WaterCommonDependencies,
+) {
+  return {
+    occupancy: dependencies.occupancy.occupancy,
+    gains_W: {},
+    energy_requirements: {},
+    water_heating: {
+      low_water_use_design: input.lowWaterUseDesign,
+      override_annual_energy_content: input.annualEnergyContentOverride !== false,
+      annual_energy_content:
+        input.annualEnergyContentOverride !== false
+          ? input.annualEnergyContentOverride
+          : undefined,
+    },
+  };
+}
+
+describe('water common', () => {
+  test('golden master', () => {
+    fc.assert(
+      fc.property(
+        arbWaterCommonInput,
+        arbWaterCommonDependencies,
+        (input, dependencies) => {
+          const waterCommon = new WaterCommon(input, dependencies);
+          const legacyData: any = makeLegacyDataForWaterCommon(input, dependencies);
+          legacyWaterHeating(legacyData);
+          expect(waterCommon.dailyHotWaterUsageMeanAnnual).toBeApproximately(
+            legacyData.water_heating.Vd_average,
+          );
+          for (const month of Month.all) {
+            expect(waterCommon.hotWaterEnergyContentByMonth(month)).toBeApproximately(
+              legacyData.water_heating.monthly_energy_content[month.index0],
+            );
+          }
+          expect(waterCommon.hotWaterEnergyContentAnnual).toBeApproximately(
+            legacyData.water_heating.annual_energy_content,
+          );
+        },
+      ),
+    );
+  });
+});
